d33d1369ea0c043d514672222d09d7846e4ff1f3
Babeena <babeenak020@gmail.com>
2023-09-06 17:28:28 +0530

    created first assignment

---
+++ ConsoleApp/ConsoleApp/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Class1
+    {
+        public void findMax(int a, int b)
+        {
+            Console.WriteLine("Maximum :"+Math.Max(a, b)); //Find the maxium among two
+            Console.WriteLine("second number is :" +  b); //Display the second integer to the screen. 
+
+
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 s1 = new Class1();
+            s1.findMax(14, 12); // calling method by passing two values
+            s1.findMax(a: 14, b: 12); // calling method specifying the parameters by name.
+        }
+}
